d90e87e7876d8ef16d44c205bd67ccbec5d79e7c
Sahil Singhai Jain <sahilsinghai5672@gmail.com>
2026-07-06 11:09:18 +0530

    fixed lazygit keymap esc and q issue; change the theme

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -8,28 +8,35 @@ return {
       "LazyGitFilter",
       "LazyGitFilterCurrentFile",
     },
+
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+
     keys = {
       { "<leader>gg", "<cmd>LazyGit<CR>", desc = "LazyGit" },
     },
-  --   config = function()
-  --     vim.api.nvim_create_autocmd("FileType", {
-  --       pattern = "lazygit",
-  --       callback = function(args)
-  --         -- Remove NvChad terminal ESC mapping for this buffer
-  --         pcall(vim.keymap.del, "t", "<Esc>", { buffer = args.buf })
-  --
-  --         -- Send real ESC to LazyGit
-  --         vim.keymap.set("t", "<Esc>", function()
-  --           vim.api.nvim_chan_send(vim.b.terminal_job_id, "\27")
-  --         end, {
-  --           buffer = args.buf,
-  --           silent = true,
-  --         })
-  --       end,
-  --     })
-  --   end,
+
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "lazygit",
+        callback = function(args)
+          -- Remove NvChad terminal ESC mapping for this buffer
+          pcall(vim.keymap.del, "t", "<Esc>", { buffer = args.buf })
+
+          -- Send real ESC to LazyGit instead of leaving terminal mode
+          vim.keymap.set("t", "<Esc>", function()
+            local chan = vim.b[args.buf].terminal_job_id
+            if chan then
+              vim.api.nvim_chan_send(chan, "\27")
+            end
+          end, {
+            buffer = args.buf,
+            silent = true,
+            desc = "Send ESC to LazyGit",
+          })
+        end,
+      })
+    end,
   },
 }
